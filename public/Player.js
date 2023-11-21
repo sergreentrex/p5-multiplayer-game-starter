9b69f2dcd9df4e7762e3a134579f215c67b73435
Sergio Zuniga <sergio@zunigamail.com>
2023-11-21 00:59:21 -0600

    message

--- public/Player.js
+++ public/Player.js
@@ -9,7 +9,7 @@ class Player {
 
   draw() {
     fill(this.rgb.r, this.rgb.g, this.rgb.b);
-    circle(this.x, this.y, 20);
+    circle(this.x, this.y, 100);
   }
   update(){
     if(keyIsDown(87)){
